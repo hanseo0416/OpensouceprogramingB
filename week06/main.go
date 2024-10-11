2ec5ed85d56a3ba44eca4c0c72669c716f5612e0
rlagkstj <dlflthf@naver.com>
2024-10-11 16:31:53 +0900

    0.6 Zero value

--- week06/main.go
+++ week06/main.go
@@ -1,19 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	i := 13
-	f := 12.9
-	c1 := 'Z'
-	c2 := '김'
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(c1, c2)    // 10진수
-	fmt.Printf("%x\n", c2) // 유니코드 '김'에 대한 16진수 출력,home.unicode.org
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+
+	i = 3
+	f = 2.7
+	fmt.Print("\n\n", f <= float64(i), "\n")
+
 }

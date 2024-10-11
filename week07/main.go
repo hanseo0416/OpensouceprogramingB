8d62cce0292cc11ec9a7f753349a9373c8305972
rlagkstj <dlflthf@naver.com>
2024-10-11 18:33:34 +0900

    v1.0 if

--- week07/main.go
+++ week07/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	var float32 float32 = 9.1
-	fmt.Println(float32)
-
+	fmt.Print("이름 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }

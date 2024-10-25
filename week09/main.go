8e849de7866936edb0a2ecafd0b35ca65f8aee4b
rlagkstj <dlflthf@naver.com>
2024-10-25 17:05:51 +0900

    v1.8

--- week09/main.go
+++ week09/main.go
@@ -16,7 +16,7 @@ func main() {
 	target := rand.Intn(6) + 1
 	fmt.Println(target)
 	for guesses := 0; guesses < 3; guesses++ {
-		fmt.Println("숫자 입력")
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
 		in := bufio.NewReader(os.Stdin)
 		input, err := in.ReadString('\n')
 		if err != nil {
@@ -25,7 +25,7 @@ func main() {
 		input = strings.TrimSpace(input)
 		guess, err := strconv.Atoi(input)
 		if target == guess {
-			fmt.Println("정답입니다.")
+			fmt.Println("정답입니다!")
 			break
 		} else if target > guess {
 			fmt.Println("입력하신 값은 정답보다 작은 수 입니다. LOW")

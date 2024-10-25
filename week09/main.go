4c3866e8c2d3056dec1c58f773a8ccdfdebbf735
rlagkstj <dlflthf@naver.com>
2024-10-25 17:01:15 +0900

    v1.7 game

--- week09/main.go
+++ week09/main.go
@@ -15,21 +15,24 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	target := rand.Intn(6) + 1
 	fmt.Println(target)
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Println("숫자 입력")
+		in := bufio.NewReader(os.Stdin)
+		input, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if target == guess {
+			fmt.Println("정답입니다.")
+			break
+		} else if target > guess {
+			fmt.Println("입력하신 값은 정답보다 작은 수 입니다. LOW")
+		} else {
+			fmt.Println("입력하신 값은 정답보다 큰 수 입니다. HIGH")
 
-	fmt.Println("숫자 입력")
-	in := bufio.NewReader(os.Stdin)
-	input, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+		}
 	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if target == guess {
-		fmt.Println("정답입니다.")
-	} else if target > guess {
-		fmt.Println("입력하신 값은 정답보다 작은 수 입니다. LOW")
-	} else {
-		fmt.Println("입력하신 값은 정답보다 큰 수 입니다. HIGH")
 
-	}
 }

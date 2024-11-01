b13b7e626cad94c8eed9c17e12afbfc6920bde06
rlagkstj <dlflthf@naver.com>
2024-11-01 17:20:43 +0900

    v2.6

--- week10/main.go
+++ week10/main.go
@@ -24,23 +24,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// counts := 0
 	var isPrime bool = true
 	if n <= 1 {
-		// counts = -1
 		isPrime = false
 	} else {
 		i := 2
 		for i < n {
 			if n%i == 0 {
-				// counts = counts + 1
 				isPrime = false
+				break
 			}
+			fmt.Printf("%d ", i)
 			i++
+
 		}
 	}
-
-	// if counts == 0 {
 	if isPrime {
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {

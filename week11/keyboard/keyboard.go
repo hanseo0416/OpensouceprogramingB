04aa31531be53315b3c1a1e7613fae5cdadb3cd7
rlagkstj <dlflthf@naver.com>
2024-11-08 18:05:37 +0900

    v3.5 Prim number

---
+++ week11/keyboard/keyboard.go
@@ -0,0 +1,41 @@
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetInteger() (int, error) {
+
+	in := bufio.NewReader(os.Stdin)
+	a, err := in.ReadString('\n')
+
+	if err != nil {
+		return 0, err
+	}
+
+	a = strings.TrimSpace(a)
+	number, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+func getFloat() (float64, error) {
+
+	in := bufio.NewReader(os.Stdin)
+	a, err := in.ReadString('\n')
+
+	if err != nil {
+		return 0, err
+	}
+
+	a = strings.TrimSpace(a)
+	number, err := strconv.ParseFloat(a, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}

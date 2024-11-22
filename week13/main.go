b5e23ce7082d9ba548695e6a76de5c9db2dfb036
rlagkstj <dlflthf@naver.com>
2024-11-22 17:00:17 +0900

    v4.3

--- week13/main.go
+++ week13/main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	var emptyslice []bool
+	emptyslice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptyslice)
+
 	gpa := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpa[1:4]
 	//gpa_slice[1] = 2.71

564657e74997193f8a2cd0610a7966ff57f27363
rlagkstj <dlflthf@naver.com>
2024-11-22 18:25:26 +0900

    v4.5

--- week13/main.go
+++ week13/main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 )
 
+func test(i int, strs ...string) {
+	//func test(strs ...string,i int) {  // error
+
+	fmt.Println(i, strs, reflect.TypeOf(strs))
+}
 func main() {
-	var emptyslice []bool
-	// emptyslice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptyslice, len(emptyslice)) //slice zero value (nil)
-	if len(emptyslice) == 0 {
-		emptyslice = append(emptyslice, true)
-	}
-	fmt.Printf("%#v %d\n", emptyslice, len(emptyslice)) //slice zero value (nil)
-
-	gpa := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpa_slice := gpa[1:4]
-	//gpa_slice[1] = 2.71
-	gpa_slice[1] = 2.71
-	gpa_slice = append(gpa_slice, false)
-	fmt.Println(len(gpa_slice), gpa_slice, gpa)
-
+	// fmt.Println(os.Args, len(os.Args), reflect.TypeOf(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices)
+	test(10, "123")
+	test(20, "123, abc")
+	test(55)
+	test(30, "123, abc, 123a")
 }
